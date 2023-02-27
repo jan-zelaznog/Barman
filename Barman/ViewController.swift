ac4198c093e0e58bedab23dcad07070e82b38f4f
c4arl0s <c.santiago.cruz@gmail.com>
2023-02-26 19:35:27 -0600

    Initial Commit

---
+++ Barman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Barman
+//
+//  Created by C4rl0s on 26/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
